9b113c7a76c1ae8c65d411374ff883acc73f2c23
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-05 16:47:46 +0100

    feat: install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  color: red;
+`;
 
 const App = () => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+  return <Container>Hello</Container>;
+};
 
 export default App;
