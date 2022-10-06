1e70d16b102343ea2380821b962c9ac2f16ec33a
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-06 08:40:23 +0100

    feat: add more info to RunnerCard

--- src/components/RunnerCard.tsx
+++ src/components/RunnerCard.tsx
@@ -1,21 +1,31 @@
 import { Card } from "react-bootstrap";
+import moment from "moment";
 
 import { Runner } from "../typings";
+import { capitalise } from "../helpers";
 
 type Props = {
   runner: Runner;
 };
 
 const RunnerCard = ({ runner }: Props) => {
+  console.log(runner);
   return (
     <div>
-      <Card>
-        <Card.Img
-          variant="top"
-          src="/default.jpeg"
-          style={{ height: "50px", width: "50px" }}
-          alt={`${runner.firstName} ${runner.lastName}`}
-        />
+      <Card style={{ height: "300px", width: "300px", marginBottom: "20px" }}>
+        <Card.Body>
+          <Card.Title>
+            {capitalise(runner.firstName)} {capitalise(runner.lastName)}
+          </Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            {runner.status}
+          </Card.Subtitle>
+          <Card.Text>£{(runner.ticketPrice.value / 100).toFixed(2)}</Card.Text>
+          <Card.Text>Race: {runner.raceTitle}</Card.Text>
+          <Card.Text>Event: {runner.eventTitle}</Card.Text>
+          <Card.Text>Event Date: {moment(runner.raceStartDate).format("MMMM Do YYYY h:mm a")}</Card.Text>
+          <Card.Link>View more information</Card.Link>
+        </Card.Body>
       </Card>
     </div>
   );
