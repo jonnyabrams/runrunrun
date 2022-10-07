5233b8ad24ce0c30ee70e0596d2ef6e4a0924ae9
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-07 08:31:45 +0100

    feat: add overall takings to Organiser page

--- src/pages/Organiser.tsx
+++ src/pages/Organiser.tsx
@@ -2,12 +2,12 @@ import { useParams } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 
 import { RunnersState } from "../context/Context";
-import { EventType, OrganiserType } from "../typings";
+import { EventType, OrganiserType, Runner } from "../typings";
 import EventCard from "../components/EventCard";
 
 const Organiser = () => {
   const { id } = useParams();
-  const { organisers, events } = RunnersState();
+  const { organisers, events, runners } = RunnersState();
 
   const organiser = organisers.find(
     (org: OrganiserType) => org.id === parseInt(id!)
@@ -17,10 +17,16 @@ const Organiser = () => {
     (event: EventType) => event.organiserId === organiser.id
   );
 
+  const overallTakings = runners
+    .filter((runner: Runner) => runner.organiserTitle === organiser.title)
+    .map((runner: Runner) => runner.ticketPrice.value)
+    .reduce((a: number, b: number) => a + b, 0);
+
   return (
     <div className="organiser_container">
       <h1>Hello, {organiser?.title || <CircularProgress size={25} />}!</h1>
-      <h2>Your Events:</h2>
+      <h2>Your Overall Takings: £{(overallTakings / 100).toFixed(2)}</h2>
+      <h3>Your Events:</h3>
       <div className="events_container">
         {organiserEvents.map((event: EventType) => (
           <EventCard event={event} />
